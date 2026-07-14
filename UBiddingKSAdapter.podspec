1914a2e050118f5e155972d56ebaa8a11453f06d
guoqiang <guoqiang.zhu@ubixai.com>
2026-07-14 15:05:48 +0800

    update UBiddingKSAdapter 5.4.10.1.2

---
+++ UBiddingKSAdapter.podspec
@@ -0,0 +1,16 @@
+
+Pod::Spec.new do |s|
+  s.name             = 'UBiddingKSAdapter'
+  s.version          = '5.4.10.1.2'
+  s.summary          = 'UBiddingKSAdapter'
+  s.homepage         = 'https://www.ubixai.com/product/md'
+  s.author           = { 'zhugq' => 'guoqiang.zhu@ubixai.com' }
+  s.source           = { :git => 'https://github.com/ubixai/UBiddingKSAdapter.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '12.0'
+
+  s.static_framework = true
+  s.vendored_frameworks = 'UBiddingKSAdapter.xcframework'
+
+  s.dependency 'KSAdSDK', '5.4.10.1'
+  s.dependency 'UBiddingAdSDK'
+end
---
+++ UBiddingKSAdapter.xcframework/ios-arm64/UBiddingKSAdapter.framework/Headers/UBiddingKSAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiddingKSAdapter.h
+//  UBiddingKSAdapter
+//
+//  Created by guoqiang.zhu on 2024/6/19.
+//  Copyright © 2024 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiddingKSAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiddingKSAdapter.
+FOUNDATION_EXPORT double UBiddingKSAdapterVersionNumber;
+
+//! Project version string for UBiddingKSAdapter.
+FOUNDATION_EXPORT const unsigned char UBiddingKSAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
